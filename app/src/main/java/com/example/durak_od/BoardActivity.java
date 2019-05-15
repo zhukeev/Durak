ffcd37400a11732062a676133bccfb63bd632188
zhukeev <djukeevar@gmail.com>
2019-05-15 23:13:54 +0600

    BottomNavigationMenu done

--- app/src/main/java/com/example/durak_od/BoardActivity.java
+++ app/src/main/java/com/example/durak_od/BoardActivity.java
@@ -1,14 +1,72 @@
 package com.example.durak_od;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BoardActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_board);
+        setContentView(R.layout.activity_menu);
+
+        init();
+        setupFAB();
+        setupBottomNavigationMenu();
     }
+
+    private void setupBottomNavigationMenu() {
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_locked:
+                        Toast.makeText(BoardActivity.this, "Locked", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void setupFAB() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BoardActivity.this, "Add", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void init() {
+        BottomAppBar bar = findViewById(R.id.bar);
+        navigationView = findViewById(R.id.bottom_navigation);
+        setSupportActionBar(bar);
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.bottom_nav_menu, menu);
+//        return true;
+//    }
+
+
 }
